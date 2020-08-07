af6725f4effc5ecd63f196c06481fe8092b9d01a
michalKoperko <fabioaru@onet.pl>
2020-08-07 22:54:36 +0200

    form php

---
+++ form.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once "connect.php";
+
+$mysqli= new mysqli($servername, $username, $password, $db_name);
+
+if($mysqli->connect_errno){
+	echo "bug connect";
+	exit();
+}
+
+else{
+	$login=$_POST['login'];
+	$passw=$_POST['password'];
+
+echo "Working";
+
+
+//$sql="SELECT  WHERE $login='nick' AND $pass='password' FROM User";
+
+$mysqli->close();
+}
+
+
+
+
+
+echo "working";
+?>
